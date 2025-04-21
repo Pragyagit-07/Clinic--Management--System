6f71c2a9c11c5d45f7128ef111e49334d44a50e5
{Pragyagit-07} <{pragyamakadariya7@gmail.com}>
2025-04-22 00:49:41 +0530

    install dotenv

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
 const doctorRoutes = require("./routes/doctorRoutes");
 const receptionistRoutes = require("./routes/receptionistRoutes");
 const billingRoutes = require("./routes/billingRoutes");
